78d20ea69e8d549030577319cfd9c2f0b62b26fc
scyangle <scyangle1314@163.com>
2019-07-25 09:29:19 +0800

    update the py

--- get_ip.py
+++ get_ip.py
@@ -6,11 +6,12 @@ import os
 
 response = requests.get("http://ip-api.com/json")
 ip_json = json.loads(response.text)
-for line in open('address.txt'):
+for line in open("/home/pi/Documents/github-pi/pi/address.txt"):
+    line = line.strip()
     if line != ip_json["query"]:
         print "need change the ip"
         os.system("echo %s > address.txt" % ip_json["query"])
         os.system("git add address.txt")
-        os.system("git commit -m %s ip update" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        os.system("git commit -m '%s ip update'" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         os.system("git push")
 print "Date:%s ip:%s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ip_json["query"])
